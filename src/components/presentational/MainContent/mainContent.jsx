a2759a7f397ff78af9f199dcc6ac1dcbd38f092a
Can Aykut <can.aykut@cz.abb.com>
2020-11-13 09:19:08 +0100

    logo and notFound page addded

--- src/components/presentational/MainContent/mainContent.jsx
+++ src/components/presentational/MainContent/mainContent.jsx
@@ -5,10 +5,11 @@ import "./mainContent.scss";
 import NavigationList from "../../container/NavigationList/navList";
 import Form from "../DevExtremeComponents/Form";
 import { Switch, Route, Link, withRouter } from "react-router-dom";
-import { Counter, TodoDetail } from "../../container";
+import { Counter, TodoDetail,WelcomePage } from "../../container";
 import { TodoTable } from "..";
 import PropTypes from "prop-types";
-import WelcomePage from "../../container/WelcomePage/welcomePage";
+import Logo from "../../../img/abb-logo.png";
+import NotFound from "../NotFound/notFound";
 
 class MainContent extends React.Component {
   constructor(props) {
@@ -32,11 +33,17 @@ class MainContent extends React.Component {
       },
     ];
 
+    this.logoRenderer= this.logoRenderer.bind(this);
     this.onOutsideClick = this.onOutsideClick.bind(this);
   }
 
   onOutsideClick() {
     this.setState({ opened: false });
+  }
+  logoRenderer () {
+
+    return (<img className="logo" src={Logo}/>);
+
   }
   static propTypes = {
     match: PropTypes.object.isRequired,
@@ -61,7 +68,10 @@ class MainContent extends React.Component {
             }}
           />
 
-          <Item
+   
+           <Item location={'after'} render={this.logoRenderer}  />     
+         {/* cssClass={'header-title'} text="ABB" */}
+             <Item
             widget='dxButton'
             location='after'
             options={{
@@ -86,6 +96,8 @@ class MainContent extends React.Component {
               <Route path='/todo' exact component={TodoTable} />
               <Route path='/todo/:id' exact component={TodoDetail} />
               <Route path='/counter' component={Counter} />
+              <Route component={NotFound} />
+
             </Switch>
           </div>
         </Drawer>
--- src/components/presentational/index.js
+++ src/components/presentational/index.js
@@ -3,4 +3,5 @@ import { format } from "prettier";
 export {default as Form} from "./DevExtremeComponents/Form";
 export {default as TodoTable} from "./Table/table";
 export {default as MainContent} from   './MainContent/mainContent';
+export {default as NotFound} from './NotFound/notFound';
 
